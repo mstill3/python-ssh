dc3dc526c63cc33af89f7895da380a644ec4a147
Matt <stillwell006@gmail.com>
2023-01-27 17:03:53 -0500

    :zap: Add read in input bash script

---
+++ res/input.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Testing comment
+
+# Testing hostname
+uname -s
+
+# Testing ls
+ls
+
+# Testing leading spaces
+    pwd
+
+# Testing sudo
+sudo ls
+
+# Testing cd
+pwd
+cd ..
+pwd
--- src/main.py
+++ src/main.py
@@ -24,22 +24,33 @@ def create_connection(host_info: HostInfo) -> Connection:
     )
 
 
-def run_commands(connection: Connection):
-    """ Run bash commands via given SSH connection """
-    commands = [
-        'uname -s',
-        'ls',
-        'pwd'
-    ]
-    show_command = True
+def execute_commands(connection: Connection, commands: [str]):
+    """ Executes bash commands via given SSH connection """
+    for index, command in enumerate(commands):
+        print("\n> " + command)
+        if command.startswith('sudo '):
+            result = connection.sudo(command.replace('sudo ', ''), hide=True)
+            print(result.stdout)
+        elif command.startswith('cd '):
+            with connection.cd(command.replace('cd ', '')):
+                execute_commands(connection, commands[index + 1:])
+            break
+        else:
+            result = connection.run(command, hide=True)
+            print(result.stdout)
 
-    for command in commands:
-        result = connection.run(command, hide=show_command)
-        print(result.stdout)
 
-        # with connection.cd('..'):
-        #     result = connection.run('ls')
-        #     print(result.stdout)
+def read_commands(file_name: str):
+    """ Form bash commands list from input file """
+    # read bash file lines
+    commands = open(file_name).read().splitlines()
+    # remove leading & trailing spaces on each line
+    commands = list(map(lambda cmd: cmd.lstrip().rstrip(), commands))
+    # remove commented out lines
+    commands = list(filter(lambda cmd: not cmd.startswith('#'), commands))
+    # remove blank lines
+    commands = list(filter(lambda cmd: not len(cmd) == 0, commands))
+    return commands
 
 
 def main():
@@ -56,7 +67,8 @@ def main():
 
     for host in hosts:
         connection = create_connection(host)
-        run_commands(connection)
+        commands = read_commands('res/input.sh')
+        execute_commands(connection, commands)
 
 
 if __name__ == '__main__':
