74a1c3d380a0c9d76c1c12497c6220d4ed9ed884
Matt <stillwell006@gmail.com>
2023-01-27 11:25:03 -0500

    :zap: Improve code

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 
-from fabric import Connection
+import getpass
+from fabric import Connection, Config
 
 
 def main():
-    print("hello world")
+    url = '127.0.0.1'
+    user = 'billy'
+    command = 'uname -s'
+    show_command = True
 
-    URL = "web1.example.com"
-    COMMAND = 'uname -s'
-    SHOW_COMMAND = True
+    password = getpass.getpass('Enter your root password: ')
+    print(password)
+    config = Config(overrides={'sudo': {'password': password}})
+    conn = Connection(url, user=user, config=config)
 
-    result = Connection(URL).run(COMMAND, hide=SHOW_COMMAND)
-    msg = "Ran {0.command!r} on {0.connection.host}, got stdout:\n{0.stdout}"
-    print(msg.format(result))
+    result = conn.run(command, hide=show_command)
+    print(result.stdout)
+    # msg = "Ran {0.command!r} on {0.connection.host}, got stdout:\n{0.stdout}"
+    # print(msg.format(result))
 
 
 if __name__ == '__main__':
